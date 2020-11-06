0e15f9db671469b0fea942876119161322674d7f
Sethu_Meena <sethumeenakaruppiah@gmail.com>
2020-11-06 17:09:32 -0500

    https issuse updated

--- timezonedata.js
+++ timezonedata.js
@@ -4,13 +4,10 @@ class TimeZoneData {
     constructor(lat,lng){
         this.apikey="SUMB42MDU1HW";
         this.lat = lat;
-        this.lng = lng;
-        var a ='one'
-        console.log(a)
+        this.lng = lng;  
     }
 
     async getTimeZoneData(){
-        console.log(a)
         const response = await fetch(`https://cors-anywhere.herokuapp.com/http://api.timezonedb.com/v2.1/get-time-zone?key=${this.apikey}&format=json&by=position&lat=${this.lat}&lng=${this.lng}`);
         const data = await response.json();
         return data; 
